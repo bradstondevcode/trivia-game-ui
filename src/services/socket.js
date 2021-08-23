4bfc0c742a6a89540ab57bd1ebe996590114de6d
Bradston Henry <bradston.henry@gmail.com>
2021-08-23 10:42:39 -0400

    Minor changes for OS deploy

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,5 @@
 import socketIOClient from "socket.io-client";
-
-const serverEndpoint = "http://localhost:8080";
+const serverEndpoint = "http://trivia-game-socket-server-a-trivia-game.trivia-test-bsh-082721-162e406f043e20da9b0ef0731954a894-0000.us-east.containers.appdomain.cloud/";
 
 export const socket = socketIOClient(serverEndpoint, {
 	transports: ['websocket']
